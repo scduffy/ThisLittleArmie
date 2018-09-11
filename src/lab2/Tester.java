3764b64d665c181853892a062ad9486e12203d4c
Chase Duffy <scduffy@loyola.edu>
2018-09-11 13:59:40 -0400

    jordan requested this

--- src/lab2/Tester.java
+++ src/lab2/Tester.java
@@ -2,5 +2,17 @@ package lab2;
 
 public class Tester
 {
-
+    /**
+    * This is our code! It’s goal is to get an a on this lab!
+    * CS 312 - Lab 2
+    * @author Driver’s Chase Duffy
+    * @author Co-Pilot’s Jordan Hettleman
+    * @version 1.0 9/11/18
+    */
+    
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World");
+    }
+    
 }
